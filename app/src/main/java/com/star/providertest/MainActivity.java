9f4974446e41ca72457d9f76b2cb0a275df21a27
crystalDf <chendong333@gmail.com>
2018-02-16 13:54:26 +0800

    Chapter 07 ProviderTest

--- app/src/main/java/com/star/providertest/MainActivity.java
+++ app/src/main/java/com/star/providertest/MainActivity.java
@@ -7,7 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,83 +25,71 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mInsertDataButton = (Button) findViewById(R.id.insert_data);
-        mInsertDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = DatabaseContract.BOOK_CONTENT_URI;
+        mInsertDataButton = findViewById(R.id.insert_data);
+        mInsertDataButton.setOnClickListener(v -> {
+            Uri uri = DatabaseContract.BOOK_CONTENT_URI;
 
-                ContentValues contentValues = new ContentValues();
+            ContentValues contentValues = new ContentValues();
 
-                contentValues.put(DatabaseContract.BOOK_COLUMN_NAME, "A Clash of Kings");
-                contentValues.put(DatabaseContract.BOOK_COLUMN_AUTHOR, "George Martin");
-                contentValues.put(DatabaseContract.BOOK_COLUMN_PAGES, 1040);
-                contentValues.put(DatabaseContract.BOOK_COLUMN_PRICE, 22.85);
+            contentValues.put(DatabaseContract.BOOK_COLUMN_NAME, "A Clash of Kings");
+            contentValues.put(DatabaseContract.BOOK_COLUMN_AUTHOR, "George Martin");
+            contentValues.put(DatabaseContract.BOOK_COLUMN_PAGES, 1040);
+            contentValues.put(DatabaseContract.BOOK_COLUMN_PRICE, 22.85);
 
-                Uri newUri = getContentResolver().insert(uri, contentValues);
+            Uri newUri = getContentResolver().insert(uri, contentValues);
 
-                if (newUri != null) {
-                    mNewId = Long.parseLong(newUri.getPathSegments().get(1));
+            if (newUri != null) {
+                mNewId = Long.parseLong(newUri.getPathSegments().get(1));
+            }
+        });
+
+        mUpdateDataButton = findViewById(R.id.update_data);
+        mUpdateDataButton.setOnClickListener(v -> {
+            Uri uri = ContentUris.withAppendedId(DatabaseContract.BOOK_CONTENT_URI, mNewId);
+
+            ContentValues contentValues = new ContentValues();
+
+            contentValues.put(DatabaseContract.BOOK_COLUMN_NAME, "A Storm of Swords");
+            contentValues.put(DatabaseContract.BOOK_COLUMN_PAGES, 1216);
+            contentValues.put(DatabaseContract.BOOK_COLUMN_PRICE, 24.05);
+
+            getContentResolver().update(uri, contentValues, null, null);
+        });
+
+        mDeleteDataButton = findViewById(R.id.delete_data);
+        mDeleteDataButton.setOnClickListener(v -> {
+            Uri uri = ContentUris.withAppendedId(DatabaseContract.BOOK_CONTENT_URI, mNewId);
+
+            getContentResolver().delete(uri, null, null);
+        });
+
+        mQueryDataButton = findViewById(R.id.query_data);
+        mQueryDataButton.setOnClickListener(v -> {
+            Uri uri = DatabaseContract.BOOK_CONTENT_URI;
+
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+            if (cursor != null) {
+
+                while (cursor.moveToNext()) {
+
+                    String name = cursor.getString(cursor.getColumnIndex(
+                            DatabaseContract.BOOK_COLUMN_NAME));
+                    String author = cursor.getString(cursor.getColumnIndex(
+                            DatabaseContract.BOOK_COLUMN_AUTHOR));
+                    int pages = cursor.getInt(cursor.getColumnIndex(
+                            DatabaseContract.BOOK_COLUMN_PAGES));
+                    double price = cursor.getDouble(cursor.getColumnIndex(
+                            DatabaseContract.BOOK_COLUMN_PRICE));
+
+                    Log.d(TAG, "Book name is " + name);
+                    Log.d(TAG, "Book author is " + author);
+                    Log.d(TAG, "Book pages is " + pages);
+                    Log.d(TAG, "Book price is " + price);
+
                 }
-            }
-        });
 
-        mUpdateDataButton = (Button) findViewById(R.id.update_data);
-        mUpdateDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = ContentUris.withAppendedId(DatabaseContract.BOOK_CONTENT_URI, mNewId);
-
-                ContentValues contentValues = new ContentValues();
-
-                contentValues.put(DatabaseContract.BOOK_COLUMN_NAME, "A Storm of Swords");
-                contentValues.put(DatabaseContract.BOOK_COLUMN_PAGES, 1216);
-                contentValues.put(DatabaseContract.BOOK_COLUMN_PRICE, 24.05);
-
-                getContentResolver().update(uri, contentValues, null, null);
-            }
-        });
-
-        mDeleteDataButton = (Button) findViewById(R.id.delete_data);
-        mDeleteDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = ContentUris.withAppendedId(DatabaseContract.BOOK_CONTENT_URI, mNewId);
-
-                getContentResolver().delete(uri, null, null);
-            }
-        });
-
-        mQueryDataButton = (Button) findViewById(R.id.query_data);
-        mQueryDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = DatabaseContract.BOOK_CONTENT_URI;
-
-                Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-
-                if (cursor != null) {
-
-                    while (cursor.moveToNext()) {
-
-                        String name = cursor.getString(cursor.getColumnIndex(
-                                DatabaseContract.BOOK_COLUMN_NAME));
-                        String author = cursor.getString(cursor.getColumnIndex(
-                                DatabaseContract.BOOK_COLUMN_AUTHOR));
-                        int pages = cursor.getInt(cursor.getColumnIndex(
-                                DatabaseContract.BOOK_COLUMN_PAGES));
-                        double price = cursor.getDouble(cursor.getColumnIndex(
-                                DatabaseContract.BOOK_COLUMN_PRICE));
-
-                        Log.d(TAG, "Book name is " + name);
-                        Log.d(TAG, "Book author is " + author);
-                        Log.d(TAG, "Book pages is " + pages);
-                        Log.d(TAG, "Book price is " + price);
-
-                    }
-
-                    cursor.close();
-                }
+                cursor.close();
             }
         });
     }
